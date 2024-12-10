96a5bee08d5b0c3ece7edb128d8f975f5bd14c6a
Alexandre Alves <alexandre_pd@hotmail.com>
2024-12-10 11:26:19 +0000

    adding cors to allow all

--- TaskFlow.API/Program.cs
+++ TaskFlow.API/Program.cs
@@ -29,8 +29,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
+
+app.UseCors();
+
 app.MapControllers();
 
 // Configure the HTTP request pipeline.
