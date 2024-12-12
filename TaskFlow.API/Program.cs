0f0c4594d8e7318e82cc502c55116d35ecc759d4
Alexandre Alves <alexandre_pd@hotmail.com>
2024-12-12 21:09:36 +0000

    Enabling beare in swagger.

--- TaskFlow.API/Program.cs
+++ TaskFlow.API/Program.cs
@@ -3,6 +3,7 @@ using TaskFlow.Application.Extensions;
 using TaskFlow.API.Extensions;
 using MediatR;
 using System.Reflection;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 // Configurar o Serilog usando a extensão
@@ -19,7 +20,41 @@ builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+    });
+
+
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement()
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "TaskFlow.API",
+        Version = "v1"
+
+    });
+}
+);
 
 // Registrar serviços da camada de Infrastructure
 builder.Services.AddInfrastructureServices(builder.Configuration);
